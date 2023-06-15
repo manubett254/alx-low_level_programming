135cc20f4dee06975895903ee404eb44a1f7e3dd
Emmanuel Bett <bettdevs@gmail.com>
2023-06-15 08:10:51 -0700

    )x00 hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+
+/*
+ * main -starting point for code
+ * body
+ * always return 0 to show success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
