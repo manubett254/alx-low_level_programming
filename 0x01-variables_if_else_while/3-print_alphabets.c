06b863054aafdf2e37013c9f801321c4c6c70d26
Emmanuel Bett <bettdevs@gmail.com>
2023-06-16 01:22:12 -0700

    0x01

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i=0;
+    for (i=97;i<=(97+25);i++)
+    {
+        putchar(i);
+    }
+    for (i=65;i<=(65+25);i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return 0;
+}
