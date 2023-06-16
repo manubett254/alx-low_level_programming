bf72fb9a560794c46a9407b51ab7de6232c2c316
Emmanuel Bett <bettdevs@gmail.com>
2023-06-16 02:45:37 -0700

    0x01

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-    int i;
-    for (i=(97+25);i>=(97);i--)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return 0;
+	int i;
+
+	for (i = (97 + 25); i >= (97); i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
